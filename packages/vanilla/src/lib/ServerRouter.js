ddb5935bb63d9ca1a7e743e4f559fa7ffd992284
hmyo2853 <hmyo2853@gmail.com>
2025-09-04 14:46:04 +0900

    Feature : 라우터 세팅

---
+++ packages/vanilla/src/lib/ServerRouter.js
@@ -0,0 +1,4 @@
+// SSG 서버 라우터
+export const ServerRouter = () => {
+  return;
+};
--- packages/vanilla/src/router/router.js
+++ packages/vanilla/src/router/router.js
@@ -1,5 +1,8 @@
 // 글로벌 라우터 인스턴스
 import { Router } from "../lib";
 import { BASE_URL } from "../constants.js";
+import { ServerRouter } from "../lib/serverRouter.js";
 
-export const router = new Router(BASE_URL);
+// window 여부에 따라서 clinet router와 server router 분기
+// BASE_URL을 serverRouter에만 전달하는 이유 : 서버 환경에서는 window객체가 없기 때문에 url정보를 불러올 수 없어 미리 명시
+export const router = typeof window !== "undefined" ? new Router() : new ServerRouter(BASE_URL);
