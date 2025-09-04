a41b753785660fcd5fcc7c0dae8ad7309b3c0784
hmyo2853 <hmyo2853@gmail.com>
2025-09-04 17:44:12 +0900

    Feature : ssg main.js에 __INITIAL_DATA__세팅 로직 추가

--- packages/vanilla/src/main.js
+++ packages/vanilla/src/main.js
@@ -4,6 +4,8 @@ import { registerAllEvents } from "./events";
 import { loadCartFromStorage } from "./services";
 import { router } from "./router";
 import { BASE_URL } from "./constants.js";
+import { productStore } from "./stores";
+import { PRODUCT_ACTIONS } from "./stores/actionTypes.js";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
@@ -16,6 +18,14 @@ const enableMocking = () =>
   );
 
 function main() {
+  // 서버 데이터 복원
+  if (window.__INITIAL_DATA__) {
+    const data = window.__INITIAL_DATA__;
+    if (data.products) productStore.dispatch(PRODUCT_ACTIONS.SETUP, data);
+    if (data.currentProduct) productStore.dispatch(PRODUCT_ACTIONS.SET_CURRENT_PRODUCT, data);
+    delete window.__INITIAL_DATA__;
+  }
+
   registerAllEvents();
   registerGlobalEvents();
   loadCartFromStorage();
